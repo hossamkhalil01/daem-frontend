0293d78ba9e4b3514b9d924033c56fb2878fb61d
Hossam Khalil <hossamkhalil001@gmail.com>
2021-06-13 23:45:22 +0200

    added the ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import PrimeButton from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
-      Daem is Running!
+
+      <h3 className="text-center"> Daem is Running!!</h3>
+      <h4 className="text-center mb-5">Testing Libraries: </h4>
+      <div className="text-center">
+        <button className="btn btn-primary mb-3 text-center">Bootstrap </button>
+        <div>
+          <PrimeButton variant="contained" color="primary">
+            Material UI
+          </PrimeButton>
+        </div>
+      </div>
     </div>
   );
 }
