752c048636a0e3d07f33edc2b4466f22dc8201dd
Hossam Khalil <hossamkhalil001@gmail.com>
2021-06-18 00:15:20 +0200

    Feature(router): created private route component

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import PrivateRoute  from "./PrivateRoute";
 import LandingPage from "../pages/LandingPage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
+
+// Test page to test the pricate routes redirection
+import PrivatePage from "../pages/PrivatePage";
 
 
 
@@ -11,7 +15,12 @@ const Router= () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/register">          
+        <Route path="/testPrivate">
+          <PrivateRoute>
+            <PrivatePage/>
+          </PrivateRoute>
+        </Route>
+        <Route path="/register">    
           <RegisterPage />
         </Route>
         <Route path="/login">          
