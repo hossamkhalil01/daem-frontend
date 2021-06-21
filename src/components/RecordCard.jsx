7616cfb126d15a7f1aee2961692f6827f1ec2744
Aya <ayahamedabdulaziz@gmail.com>
2021-06-21 22:22:18 +0200

    feature(record-card): Redirect to ticket page option.

--- src/components/RecordCard.jsx
+++ src/components/RecordCard.jsx
@@ -1,6 +1,4 @@
-import {
-    default as Button
-} from "@material-ui/core/Button";
+import { default as Button } from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
@@ -8,13 +6,14 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router";
 import { formatDate } from "../services/dateService";
 import "../styles/RecordCard.css";
 
-  
 export default function RecordCard({ ticket }) {
   const [openModal, setOpenModal] = useState(false);
   const { t } = useTranslation();
+  const history = useHistory();
 
   const handleClickOpen = () => {
     setOpenModal(true);
@@ -22,13 +21,16 @@ export default function RecordCard({ ticket }) {
   const handleCancel = () => {
     setOpenModal(false);
   };
+
+  const handleTicketRedirection = () => history.push(`/ticket/${ticket._id}`);
+
   return (
     <>
       <div onClick={handleClickOpen} className="record-card">
         <div className="record__header">
           {" "}
           <span>{ticket.subject} </span>{" "}
-          <span className="record__date">{formatDate(ticket.createdAt)}</span>
+          <span className="record__date">{formatDate(ticket.createdAt, t("language"))}</span>
         </div>
         <div className="record__body">
           <p>{ticket.description}</p>
@@ -47,6 +49,9 @@ export default function RecordCard({ ticket }) {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
+          <Button onClick={handleTicketRedirection} color="primary">
+            {t("go-to-ticket")}
+          </Button>
           <Button onClick={handleCancel} color="primary" autoFocus>
             {t("close")}
           </Button>
