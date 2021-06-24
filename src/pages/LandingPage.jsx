8c4bbcded1762e874c918b941c69589aa7cb8250
Montaser <abdelrahmanmontaser12345@gmail.com>
2021-06-24 16:09:46 +0200

    Refact(date-filter): remove unneeded code

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { Filter } from "../components/Filter";
 const LandingPage = (props) => {
   return (
-    <>
-      <Filter />
       <h3> Landing Page </h3>
-    </>
   );
 };
 
