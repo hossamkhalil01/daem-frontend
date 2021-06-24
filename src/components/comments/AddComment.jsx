ca48af641363f0570458ebe9293edec3cf722aca
Montaser <abdelrahmanmontaser12345@gmail.com>
2021-06-24 18:40:02 +0200

    Feat(Comments): add create comments form

---
+++ src/components/comments/AddComment.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+export const AddComments = ({ ticketId }) => {
+  const [comment, updateComment] = useState("");
+  const [error, setError] = useState("");
+  const handleCommentSubmit = async () => {
+    if (comment === "") return setError("you can't add empty comment ");
+  };
+
+  const upadteInputStates = () => {
+    setError("");
+  };
+
+  return (
+    <div className="row align-baseline">
+      <div className="col-md-10">
+        <textarea
+          onBlur={() => upadteInputStates()}
+          value={comment}
+          onChange={(e) => updateComment(e.target.value)}
+          className="form-control"
+        ></textarea>
+      </div>
+      <div className="col-md-2">
+        <button
+          onClick={() => handleCommentSubmit()}
+          className="btn btn-success"
+        >
+          Add
+        </button>
+      </div>
+      {error === "" ? (
+        ""
+      ) : (
+        <div className="row justify-content-center">
+          <div className="col-md-9 alert alert-danger mt-3">{error}</div>
+          <div className="col-md-2"></div>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import {AddComments} from '../components/comments/AddComment';
 const LandingPage = (props) => {
 
     return (
+      <>
+      <AddComments />
       <h3> Landing Page </h3>
+      </>
     );
 };
 
