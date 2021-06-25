831b9f9ecd84d1d2b7caddc546488503c6a4f7f7
Aya <ayahamedabdulaziz@gmail.com>
2021-06-25 23:20:39 +0200

    feature(ticket-page): Display actions and medical record based on current user role

--- src/components/ticketActions/TicketActions.jsx
+++ src/components/ticketActions/TicketActions.jsx
@@ -1,18 +1,38 @@
+import { getUser } from "../../services/authService";
 import "../../styles/TicketActions.css";
 import AssignDoctor from "./AssignDoctor";
 import CheckTicket from "./CheckTicket";
 import DeleteTicket from "./DeleteTicket";
 import EditTicket from "./EditTicket";
 import SetUrgency from "./SetUrgency";
-export default function TicketActions({ ticket }) {
-  return (
-    <>
-      <SetUrgency ticketId={ticket._id} ticketUrgency={ticket.urgency} />{" "}
-      <AssignDoctor  ticketId={ticket._id} ticketDoctor={ticket.doctor} />
-      <EditTicket ticketId={ticket._id} />
-      <DeleteTicket ticketId={ticket._id} />
-      <CheckTicket ticketId={ticket._id} isChecked={ticket.isChecked} />
 
-    </>
-  );
+export default function TicketActions({ ticket }) {
+  const user = getUser();
+
+  if (user.role === "doctor") {
+    return (
+      <>
+        <SetUrgency ticketId={ticket._id} ticketUrgency={ticket.urgency} />
+        <AssignDoctor ticketId={ticket._id} ticketDoctor={ticket.doctor} />
+      </>
+    );
+  } else if (user.role === "moderator") {
+    return (
+      <>
+        <SetUrgency ticketId={ticket._id} ticketUrgency={ticket.urgency} />
+        <AssignDoctor ticketId={ticket._id} ticketDoctor={ticket.doctor} />
+        <DeleteTicket ticketId={ticket._id} />
+        <CheckTicket ticketId={ticket._id} isChecked={ticket.isChecked} />
+      </>
+    );
+  } else if (user._id === ticket.patient._id && ticket.state === "unresolved") {
+    return (
+      <>
+        <EditTicket ticketId={ticket._id} />
+        <DeleteTicket ticketId={ticket._id} />
+      </>
+    );
+  } else {
+    return <></>;
+  }
 }
