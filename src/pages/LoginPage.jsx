1047a4aca0bfeebb89a1a1f8d8f8850d1a7b2871
Hossam Khalil <hossamkhalil001@gmail.com>
2021-06-26 22:16:02 +0200

    Feature(login-page): created login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Login from "../components/auth/Login";
+import Navbar from "../components/layouts/Navbar";
+import Footer from "../components/layouts/Footer";
 
 const LoginPage = (props) => {
-  return <Login />;
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Login />
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default LoginPage;
