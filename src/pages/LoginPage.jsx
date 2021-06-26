ed24bff0802036ebcd38f9a52895a668d6f143b0
Hossam Khalil <hossamkhalil001@gmail.com>
2021-06-26 22:19:14 +0200

    Feature(login-page): redirect to home if already logged in

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Login from "../components/auth/Login";
 import Navbar from "../components/layouts/Navbar";
 import Footer from "../components/layouts/Footer";
+import { getUser } from "../services/authService";
 
 const LoginPage = (props) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    // check if already logged in
+    if (getUser())
+      // redirect to home
+      history.push("/");
+  }, []);
+
   return (
     <>
       <Navbar />
